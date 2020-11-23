d5dcb53e2039ce195068ec541030c17b198e8bf0
unknown <guymdls1508@gmail.com>
2020-11-23 14:11:51 +0200

    adding select format

--- src/Palette.js
+++ src/Palette.js
@@ -9,21 +9,30 @@ import Navbar from "./Navbar"
 class Palette extends Component {
     constructor(props) {
         super(props);
-        this.state = { level: 500 }
+        this.state = { level: 500, format: "hex" }
         this.changeLevel = this.changeLevel.bind(this)
+        this.changeFormat = this.changeFormat.bind(this)
 
     }
     changeLevel(newLevel) {
         this.setState({ level: newLevel })
     }
+
+    changeFormat(value) {
+        this.setState({ format: value });
+    }
     render() {
         const colorBoxes = this.props.palette.colors[this.state.level].map(color => {
-            return <ColorBox background={color.hex} name={color.name} />
+            return <ColorBox background={color[this.state.format]} name={color.name} />
         });
 
         return (
             <div className='Palette'>
-                <Navbar level={this.state.level} changeLevel={this.changeLevel} />
+                <Navbar
+                    level={this.state.level}
+                    changeLevel={this.changeLevel}
+                    handleChange={this.changeFormat}
+                />
                 <div className='Palette-colors'>
                     {colorBoxes}
                 </div>
